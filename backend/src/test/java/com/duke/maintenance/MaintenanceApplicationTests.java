e6965c18cf2bcb5a3243bc23072d4108a167e1f9
Bautisad <duke.bautista@colostate.edu>
2026-07-25 12:40:34 -0600

    Fix tests database config

--- backend/src/test/java/com/duke/maintenance/MaintenanceApplicationTests.java
+++ backend/src/test/java/com/duke/maintenance/MaintenanceApplicationTests.java
@@ -2,12 +2,13 @@ package com.duke.maintenance;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MaintenanceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
