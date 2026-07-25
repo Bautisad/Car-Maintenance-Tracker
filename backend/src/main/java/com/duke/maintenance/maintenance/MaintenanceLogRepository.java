523fce419f840cad38aa05dda72f6957498ed9d2
Bautisad <duke.bautista@colostate.edu>
2026-07-25 12:12:01 -0600

    Added MaintenanceLogRepositoryTest class

--- backend/src/main/java/com/duke/maintenance/maintenance/MaintenanceLogRepository.java
+++ backend/src/main/java/com/duke/maintenance/maintenance/MaintenanceLogRepository.java
@@ -1,5 +1,10 @@
 package com.duke.maintenance.maintenance;
 
-public class MaintenanceLogRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MaintenanceLogRepository extends JpaRepository<MaintenanceLog, Long> {
+
+    List<MaintenanceLog> findByVehicleId(Long vehicleId);
 }
